f54968a8e1d059817aebf07e27156297d6162e8b
Tatiana Kudria <94138737+TatianaKud@users.noreply.github.com>
2022-10-04 19:37:55 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-const board = document.querySelector('#board')
-const colors = ['#ffb3ba', '#ffdfba', '#ffffba', '#baffc9', '#bae1ff']
+const board = document.querySelector("#board")
+const colors = ["#ffb3ba", "#ffdfba", "#ffffba", "#baffc9", "#bae1ff"]
 const SQUARES_NUMBER = 400
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
-    const square = document.createElement('div')
-    square.classList.add('square')
+  const square = document.createElement("div")
+  square.classList.add("square")
 
-    square.addEventListener('mouseover', () => 
-    setColor(square))
+  square.addEventListener("mouseover", setColor)
 
-    square.addEventListener('mouseleave', () => 
-    removeColor(square))
+  square.addEventListener("mouseleave", removeColor)
 
-    board.append(square)
+  board.append(square)
 }
 
-function setColor(element) {
-    const color = getRandomColor()
-    element.style.backgroundColor = color
-    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+function setColor(event) {
+  const element = event.target
+  const color = getRandomColor()
+  element.style.backgroundColor = color
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(element) {
-    element.style.backgroundColor = '#1d1d1d'
-    element.style.boxShadow = `0 0 2px #000`
+function removeColor(event) {
+  const element = event.target
+  element.style.backgroundColor = "#1d1d1d"
+  element.style.boxShadow = `0 0 2px #000`
 }
 
 function getRandomColor() {
-   const index = Math.floor(Math.random() * colors.length)
-    return colors[index]
+  return colors[Math.floor(Math.random() * colors.length)]
 }
